c516b6df24a790a212442e00f7b0ad6f3fa7e5b8
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-10-14 21:40:25 -0700

    Introduce randomness

--- coin_flipper/coin_flipper.ino
+++ coin_flipper/coin_flipper.ino
@@ -1,29 +1,52 @@
 int heads_led_pin = A0;
 int tails_led_pin = A1;
+int winner_pin;
 int buzzer_pin = A2;
-int buzzer_time = 200;
+int buzzer_time = 100;
 int led_time = 1000;
+long random_number;
 
 void setup() {
+  randomSeed(analogRead(0));
   pinMode(heads_led_pin, OUTPUT);
   pinMode(tails_led_pin, OUTPUT);
   pinMode(buzzer_pin, OUTPUT);
+
+  for (int i = 25; i >= 1; i--) {
+    digitalWrite(buzzer_pin, HIGH);
+    digitalWrite(heads_led_pin, LOW);
+    digitalWrite(tails_led_pin, LOW);
+    delay(buzzer_time);
+    digitalWrite(buzzer_pin, LOW);
+    digitalWrite(heads_led_pin, HIGH);
+    delay(led_time / i);
+    digitalWrite(buzzer_pin, HIGH);
+    digitalWrite(heads_led_pin, LOW);
+    digitalWrite(tails_led_pin, LOW);
+    delay(buzzer_time);
+    digitalWrite(buzzer_pin, LOW);
+    digitalWrite(tails_led_pin, HIGH);
+    delay(led_time / i);
+  }
+
+  digitalWrite(heads_led_pin, LOW);
+  digitalWrite(tails_led_pin, LOW);
+  digitalWrite(buzzer_pin, HIGH);
+  delay(buzzer_time * 3);
+  digitalWrite(buzzer_pin, LOW);
+
+  random_number = random(2);
+
+  if (random_number == 0) {
+    winner_pin = heads_led_pin;
+  } else {
+    winner_pin = tails_led_pin;
+  }
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(buzzer_pin, HIGH);
-  digitalWrite(heads_led_pin, LOW);
-  digitalWrite(tails_led_pin, LOW);
-  delay(buzzer_time);
-  digitalWrite(buzzer_pin, LOW);
-  digitalWrite(heads_led_pin, HIGH);
+  digitalWrite(winner_pin, LOW);
   delay(led_time);
-  digitalWrite(buzzer_pin, HIGH);
-  digitalWrite(heads_led_pin, LOW);
-  digitalWrite(tails_led_pin, LOW);
-  delay(buzzer_time);
-  digitalWrite(buzzer_pin, LOW);
-  digitalWrite(tails_led_pin, HIGH);
+  digitalWrite(winner_pin, HIGH);
   delay(led_time);
 }
